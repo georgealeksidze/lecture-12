af26d3d1ac4e01a6fedfe3e4c38aaf8661b1201d
georgealeksidze <georgealeksidze@hotmail.com>
2025-12-02 17:56:09 +0400

    start

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// ტიპები //
+// 30 number
+
+// "Niki" String
+
+// true/false boollean
+
+// bigInt 
+
+// symbol 
+
+// undefined ჯავაში არ არის ჯერჯერობით კოდი
+
+// null ჩვენთვითონ გავუწერთ, რომ მნიშვნელობა არააქვს
+
+// [] array მასივი
+
+// obj {} ობიექტი
+
+
+console.log ("Hello World");
+
+console.error ("Hello World")
+
+console.warn ("Hello World")
